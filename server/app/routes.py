87482618568aa459da9209c48b396fd0fb490df5
Matthew Bloch <mjbloch22@gmail.com>
2023-08-12 14:41:53 -0700

    server updates to use google cloud storage for images

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from flask import render_template, request
+from google.cloud import storage
 import os
 from werkzeug.utils import redirect, secure_filename
 
@@ -6,8 +8,16 @@ from app import app, APP_ROOT
 from app.process import catdog_predict
 
 
-upload_folder = os.path.join(APP_ROOT, 'static', 'uploads')
-app.config['UPLOAD'] = upload_folder
+# upload_folder = os.path.join(APP_ROOT, 'static', 'uploads')
+
+# FOR LOCAL TESTING ONLY
+credentials = "catdog-395720-fda24c2997ae.json"
+bucket_name = "catdog-images"
+
+# bucket_name = os.environ.get("BUCKET_NAME")
+storage_client = storage.Client.from_service_account_json(credentials, project='catdog-395720')
+
+# app.config['UPLOAD'] = upload_folder
 
 
 @app.route('/')
@@ -29,16 +39,23 @@ def about():
 def upload():
     # Get the uploaded file from the request
     if request.method == 'POST':
-        file = request.files['img']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD'], filename))
+        image = request.files['img']
+        filename = secure_filename(image.filename)
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(filename)
+        blob.upload_from_string(image.read(), content_type=image.content_type)
         return redirect('/prediction/{}'.format(filename))
 
 
 @app.route("/prediction/<filename>", methods=['GET', 'POST'])
 def prediction(filename):
-    prediction_probs, prediction_name = catdog_predict(filename) #imported from process file
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+    image_url = blob.generate_signed_url(
+        expiration = timedelta(minutes=5),
+        method='GET'
+    )
+    prediction_probs, prediction_name = catdog_predict(image_url) #imported from process file
     prob_cat = round(prediction_probs[0] * 100, 2)
     prob_dog = round(prediction_probs[1] * 100, 2)
-    image_path = 'uploads' + '/' + filename
-    return render_template('output.html', prob_cat = prob_cat, prob_dog = prob_dog, name = prediction_name, prediction_image = image_path)
+    return render_template('output.html', prob_cat = prob_cat, prob_dog = prob_dog, name = prediction_name, prediction_image = image_url)
